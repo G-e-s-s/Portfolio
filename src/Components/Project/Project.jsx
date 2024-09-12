47209f281f6d7835f0fe8dce1633c3a8763fb2d6
Gessica Goublet <gess_06570@hotmail.fr>
2024-09-12 19:23:56 +0200

    Création projets

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -4,21 +4,29 @@ import projectsInfo from '../../assets/Projects.json';
 // import "../../Pages/Projects/Projects.css";
 
 const Project = () => {
+  console.log(projectsInfo);
   return (
     <div className="projects" >
       <h2>My Projects</h2> 
-      <div className="project">
-          <h2>{projectsInfo.name}</h2>
-          <p>{projectsInfo.desc}</p>
-        <div className="tech-stack">
-          {projectsInfo.techs} 
-        </div>
-        <div className="btns">
-          <a href={projectsInfo.liveLink} target="_blank" rel="noreferrer">Live</a>
-          <a href={projectsInfo.githubLink} target="_blank" rel="noreferrer">Github</a>
-        </div>
-        {projectsInfo.isOpenSource && <div className="open">Open Source</div>}
-      </div>  
+      <div>
+      {projectsInfo.map((project) => (
+        <div className="project">
+          <h2>{project.name}</h2>
+          <p>{project.desc}</p>
+          <div className="tech-stack">
+            {project.techs.map((tech) => (
+              <div>{tech}</div>
+            ))} 
+          </div>
+          <div className="btns">
+            <a href={project.liveLink} target="_blank" rel="noreferrer">Live</a>
+            <a href={project.githubLink} target="_blank" rel="noreferrer">Github</a>
+          </div>
+          {project.isOpenSource && <div className="open">Open Source</div>}
+          </div>
+      ))} 
+      </div>
+      
     </div>
   )
 }
