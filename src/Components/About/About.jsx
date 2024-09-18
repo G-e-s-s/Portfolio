8053ff44af59d42d6c1732b718f89ae50479583d
Gessica Goublet <gess_06570@hotmail.fr>
2024-09-18 09:36:27 +0200

    Ajout description

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -7,9 +7,10 @@ const About = () => {
     <div className='about' >
       <h2 id="about">À propos de moi</h2>
       <div className="words">
-      Basé à Rennes, je suis intéressée par le développement front-end, le web design et le graphisme. <br></br>
+      Je suis intéressée par le développement front-end, le web design et le graphisme. <br></br>
       Étant créative et passionnée, j’aspire à devenir UX/UI designer pour développer des projets centrés autour de l'Humain, tout en apportant des solutions bénéfiques à l'expérience utilisateur et l'accessibilité. <br></br>
-      De l’identité visuelle, au développement des sites web, j’allie innovation, design et performance.
+      De l’identité visuelle, au développement des sites web, j’allie innovation, design et performance. <br></br>
+      Vous pouvez me retrouver à Rennes, mais aussi en distanciel.
       </div>
     </div>
   )
